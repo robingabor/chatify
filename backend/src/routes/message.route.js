d2c57894d91e70f31462839ff31f310d9483d8cf
robingabor <grobin03@hotmail.com>
2025-12-02 21:19:48 +0100

    initial commit

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/send', (req, res) => {
+    res.send('Messages endpoint');
+});
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;   
+// lets use the auth, this is an application level middleware
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
